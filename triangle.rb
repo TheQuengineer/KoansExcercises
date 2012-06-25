c7bfd7b30da1b3419a689358d4711280e01944c5
Quentin <qmanthomas@gmail.com>
2012-06-25 09:34:09 -0400

    Excercise Answers

---
+++ triangle.rb
@@ -0,0 +1,35 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+  
+   raise TriangleError, "Sides have to be greater than zero" if (a == 0) | (b == 0) | (c == 0)
+   raise TriangleError, "Sides have to be a postive number" if (a < 0) | (b < 0) | (c < 0)
+   raise TriangleError, "Two sides can never be less than the sum of one side" if ((a + b) < c) | ((a + c) < b) | ((b + c) < a)
+   raise TriangleError, "Two sides can never be equal one side" if ((a + b) ==  c) | ((a + c) ==  b) | ((b + c) ==  a)
+   
+  if a==b && a==c
+    :equilateral
+  elsif (a==b && a!=c) || (a==c && a!=b) || (b==c && b!=a)
+    :isosceles
+  else 
+    :scalene 
+  end 
+       
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
